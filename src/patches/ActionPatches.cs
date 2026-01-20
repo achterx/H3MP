8a4cf9bd4a364be9182622698393679bd74732c1
achter <90976703+achterx@users.noreply.github.com>
2026-01-20 22:47:39 +0300

    Update ActionPatches.cs

--- src/patches/ActionPatches.cs
+++ src/patches/ActionPatches.cs
@@ -3926,6 +3926,7 @@ namespace H3MP.Patches
             }
             return true;
     }
+    }
 
     // Patches SosigInventory update methods to prevent processing on non controlling client
     class SosigInvUpdatePatch
